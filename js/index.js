b1f976e6b4af109ad13421a839822cc0a0d7ab28
ArtZagvozkin <artzagvozkin@yandex.ru>
2022-05-12 17:07:53 +0300

    add url filter

--- js/index.js
+++ js/index.js
@@ -4,20 +4,44 @@ function load_page() {
     feedback_err.style.display = "none";
 }
 
-
 function feedback_send() {
     var input = document.getElementById('feedback-txtbx').value;
-    var regexp = /<\/?[a-zA-Zа-яА-Я .:/\\"'!?=”)]+>/gi;
+    var message = "";
 
-    if (String(input).match(regexp) == null) {
-        document.getElementById('feedback-err').style.display = "none";
-    } else {
-        document.getElementById('feedback-err').style.display = "inherit";
+    //tag filter
+    if (String(input).match(/<.+>/gi) != null) {
+        message = "Warn: Форма ввода содержит теги!!!";
+    }
+    input = input.replace(/<.+>/gi,'');
+
+    //filter forbidden words
+    var result = input;
+    result = result.replace(/баба/gi, '');
+    if (result != input) {
+        message += message !== "" ? "<br/>" : "";
+        message += "Warn: Форма ввода содержит запрещенные слова!!!";
+        input = result;
     }
 
-    var result = input.replace(regexp,'');
-    var feedback_out = document.getElementById('feedback-out');
-    feedback_out.innerHTML = result;
+    //url filter
+    result = input.replace(/http:\/\/\S{0,}/gi, '');
+    result = result.replace(/https:\/\/\S{0,}/gi, '');
+    if (result != input) {
+        message += message !== "" ? "<br/>" : "";
+        message += "Warn: Форма ввода содержит ссылки!!!";
+        input = result;
+    }
+
+    //write to feedback-out
+    document.getElementById('feedback-out').innerHTML = input;
+
+    //error box
+    if (message != "") {
+        document.getElementById('feedback-err').style.display = "inherit";
+        document.getElementById('feedback-err').innerHTML = message;
+    } else {
+        document.getElementById('feedback-err').style.display = "none";
+    }
 }
 
 function img_mouse_over(img_id) {
